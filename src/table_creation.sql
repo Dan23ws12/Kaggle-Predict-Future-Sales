d536feb3d5d231b6635a6f4e9bcf224ced197b48
Dan23ws12 <danieloyeleye247@gmail.com>
2026-06-26 11:24:31 -0400

    changed some table definitions

--- src/table_creation.sql
+++ src/table_creation.sql
@@ -19,9 +19,9 @@ CREATE TABLE items(
 
 );
 
-CREATE TABLE original_train_tb(
+CREATE TABLE orig_sales_train(
     sales_id: int PRIMARY KEY, 
-    sales_date: date, 
+    sales_date: varchar(10), 
     sales_date_block_num: int,
     item_price: int,
     item_cnt_day: int,
@@ -30,8 +30,9 @@ CREATE TABLE original_train_tb(
     item_category_id: int references item_categories(item_category_id)
 
 );
--- same as training data table except item_cnt_day is the absolute of it's real value
-CREATE TABLE train_tb_abs_cnt(
+-- the transformed version of orig_sales_train used to train the machine learning models
+-- for answer submission
+CREATE TABLE final_sales_train(
     sales_id: int PRIMARY KEY, 
     sales_date: date, 
     sales_date_block_num: int,
