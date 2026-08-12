0d8e69c6a01eb524b4c4d57c06f52b23392a6e71
Dan23ws12 <danieloyeleye247@gmail.com>
2026-08-12 13:26:40 -0400

    replaced misspelled item_month with item_cnt_month

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,6 +1,6 @@
 CAT_FEATURES:list[str] = ["month_block_num", "shop_id", "item_id", "item_category_id"]
 CAT_COLS:list[str] = ["month_block_num", "shop_id", "item_id", "item_category_id"]
-NUMERIC_COLS:list[str] = ["item_price", "item_month"]
+NUMERIC_COLS:list[str] = ["item_price", "item_cnt_month"]
 NUMERIC_FEATURES:list[str] = ["item_price"]
 RAND_STATE:int = 35
 TARGET_COL:str = "item_cnt_month"
