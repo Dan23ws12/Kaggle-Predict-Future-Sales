0460f8b90d3a603e33983f3139dc5b599fcf755e
Dan23ws12 <danieloyeleye247@gmail.com>
2026-08-12 12:27:14 -0400

    moved common variable definitions in helpers/ files to __init__.py

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,1 +1,9 @@
-__all__ = ['epa_helpers', "feature_eng_helpers"]
+CAT_FEATURES:list[str] = ["month_block_num", "shop_id", "item_id", "item_category_id"]
+CAT_COLS:list[str] = ["month_block_num", "shop_id", "item_id", "item_category_id"]
+NUMERIC_COLS:list[str] = ["item_price", "item_month"]
+NUMERIC_FEATURES:list[str] = ["item_price"]
+RAND_STATE:int = 35
+TARGET_COL:str = "item_cnt_month"
+
+
+__all__ = ['epa_helpers', "feature_eng_helpers", CAT_FEATURES, NUMERIC_COLS, NUMERIC_FEATURES, RAND_STATE, TARGET_COL]
