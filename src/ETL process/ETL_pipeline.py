3436512eb31e4f8fb685696e442c35a750eb223d
Dan23ws12 <danieloyeleye247@gmail.com>
2026-08-19 12:56:19 -0400

    moved functions for loading data into csvs from ETL_pipeline to load_to_csv file

--- src/ETL process/ETL_pipeline.py
+++ src/ETL process/ETL_pipeline.py
@@ -96,30 +96,5 @@ def load_full_data_to_csv(sales: pd.DataFrame, table_name:str):
 def load_train_data_to_csv(train_data: pd.DataFrame, table_name:str):
     """This function loads the training dataset to the csv files"""
     train_data.to_csv(os.getenv('CLEAN_DATA_PATH') + '/' + table_name + '.csv', index=False)
-
-
-if __name__ == "__main__":
-    #importing the .env variables
-    load_dotenv()
-    # imports original sales data
-    orig_sales_data = data_extraction("sales_train")
-    items_data = data_extraction("items")[["item_id", "item_category_id"]]   
-    # creates template for training data
-    sales_df_template = get_train_data_template(orig_sales_data)
-    # names of dataframes to be created
-    df_table_names = {
-        "zero": "sales_train_cnt_0",
-        "abs": "sales_train_cnt_abs",
-        "mean": "sales_train_cnt_mean",
-        "bfill": "sales_train_cnt_bfill",
-        "ffill": "sales_train_cnt_ffill"
-    }
-    #loops through the names of the dataframes and loads the training data to csv
-    for key, value in df_table_names.items():
-        sales_filled_df = get_sales_train_data(sales_df_template, fill_method=key)
-        if (key == "zero"):
-            load_full_data_to_csv(get_full_sales_data(sales_filled_df, items_data), "sales_full_0")
-        load_train_data_to_csv(sales_filled_df, value)
-    
     
     
---
+++ src/ETL process/load_to_csv.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from ETL_pipeline import data_extraction, get_train_data_template, get_sales_train_data, get_full_sales_data
+
+load_dotenv()
+
+def load_full_data_to_csv(sales: pd.DataFrame, table_name:str):
+    """This function loads the full dataset to the csv files"""
+    sales.to_csv(os.getenv('FULL_DATA_PATH') + '/' + table_name + '.csv', index=False)
+
+def load_train_data_to_csv(train_data: pd.DataFrame, table_name:str):
+    """This function loads the training dataset to the csv files"""
+    train_data.to_csv(os.getenv('CLEAN_DATA_PATH') + '/' + table_name + '.csv', index=False)
+
+if __name__ == "__main__":
+    #importing the .env variables
+    load_dotenv()
+    # imports original sales data
+    orig_sales_data = data_extraction("sales_train")
+    items_data = data_extraction("items")[["item_id", "item_category_id"]]   
+    # creates template for training data
+    sales_df_template = get_train_data_template(orig_sales_data)
+    # names of dataframes to be created
+    df_table_names = {
+        "zero": "sales_train_cnt_0",
+        "abs": "sales_train_cnt_abs",
+        "mean": "sales_train_cnt_mean",
+        "bfill": "sales_train_cnt_bfill",
+        "ffill": "sales_train_cnt_ffill"
+    }
+    #loops through the names of the dataframes and loads the training data to csv
+    for key, value in df_table_names.items():
+        sales_filled_df = get_sales_train_data(sales_df_template, fill_method=key)
+        if (key == "zero"):
+            load_full_data_to_csv(get_full_sales_data(sales_filled_df, items_data), "sales_full_0")
+        load_train_data_to_csv(sales_filled_df, value)
