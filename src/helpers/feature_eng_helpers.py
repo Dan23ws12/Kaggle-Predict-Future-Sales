6fe08948ac5d46f022d1ad4f11afceddbb013605
Dan23ws12 <danieloyeleye247@gmail.com>
2026-08-24 11:10:28 -0400

    removed all imports constants except RAND_STATE, changed train_random_forest to return only the model (no more cross validation) in feature_eng_helpers

--- src/helpers/feature_eng_helpers.py
+++ src/helpers/feature_eng_helpers.py
@@ -4,7 +4,8 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor
 from .data_preprocessor import SalesDataPreprocessor
-from . import CAT_FEATURES, NUMERIC_FEATURES, RAND_STATE, TARGET_COL
+from . import RAND_STATE
+from typing import Optional
 
 data_preprocessor = SalesDataPreprocessor()
 
@@ -20,21 +21,9 @@ def preprocess_and_split_data(data: pd.DataFrame) -> tuple[pd.DataFrame]:
     new_data = new_data.drop(columns=["date"])
     return data_preprocessor.split_data(new_data)
 
-def train_random_forest(x_train, y_train):
-    param_grid = {
-        "n_estimators": [70, 100, 150],
-        "min_samples_leaf": [1, 2, 5],
-        "max_depth": [None, 9, 11]
-    }
-
-    rand_forest = RandomForestRegressor(random_state=RAND_STATE)
-
-    grid_search = GridSearchCV(estimator=rand_forest, param_grid=param_grid, 
-        cv=10, n_jobs=-1
-    )
-    grid_search.fit(x_train, y_train)
-    print(f"best score of cv is {grid_search.best_score_}")
-    print("best parameters")
-    print(grid_search.best_params_)
-    return grid_search.best_estimator_
+def train_random_forest(n_estimators: int, min_samples_leaf: int, max_depth: Optional[int] = None):
+    rand_forest = RandomForestRegressor(random_state=RAND_STATE, n_jobs=-1, 
+        n_estimators=n_estimators, min_samples_leaf=min_samples_leaf, 
+        max_depth=max_depth, verbose=1)
+    return rand_forest
 
