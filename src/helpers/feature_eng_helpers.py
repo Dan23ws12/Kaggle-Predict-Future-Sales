d235dda2cd546205472230cef6b1097d006d426b
Dan23ws12 <danieloyeleye247@gmail.com>
2026-08-26 11:47:19 -0400

    aggregated over data to reduce data size

--- src/helpers/feature_eng_helpers.py
+++ src/helpers/feature_eng_helpers.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from .data_preprocessor import SalesDataPreprocessor
+from . import TARGET_COL
 
 data_preprocessor = SalesDataPreprocessor()
 
@@ -11,7 +12,21 @@ def preprocess_and_split_data(data: pd.DataFrame) -> tuple[pd.DataFrame]:
     """
     data = data_preprocessor.replace_infrequent_values(data)
     data = data_preprocessor.add_features(data)
-    return data_preprocessor.split_data(data)
+    keys = ["shop_id", "item_id", "month_block_num"]
+    #aggregate to drop daily item_price and item_cnt_day rows and
+    # to reduce data size
+    data_grouped = data.groupby(keys, as_index=False).agg({
+        TARGET_COL: "first",
+        "month_block_length": "first",
+        "item_name_length": "first",
+        "item_months_sold": "first",
+        "avg_item_price_per_month": "first",
+        "avg_sales_per_shop": "first",
+        "avg_sales_per_item": "first",
+        "item_price_mean": "mean", 
+        "item_price_median": "median",
+    })
+    return data_preprocessor.split_data(data_grouped)
 
 def get_preprocessed_data(data: pd.DataFrame) -> pd.DataFrame:
     """
